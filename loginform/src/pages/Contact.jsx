36472da0c2b38742549ef946d2ac339f3a22a986
rupeshbodineni <rupeshbodineni@gmail.com>
2026-07-21 10:11:02 +0530

    added contact page

---
+++ loginform/src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+export default function Contact() {
+  return (
+    <div className="container mt-5">
+      <h1>Contact Us</h1>
+      <p>This is the Contact page of our application.</p>
+      <p>Get in touch with us at contact@example.com</p>
+    </div>
+  )
+}
