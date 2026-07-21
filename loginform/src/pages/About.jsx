eda910da6eebbc64732c9382405911366c718144
rupeshbodineni <rupeshbodineni@gmail.com>
2026-07-21 10:11:41 +0530

    added aboutpage

---
+++ loginform/src/pages/About.jsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <div className="container mt-5">
+      <h1>About Us</h1>
+      <p>This is the About page of our application.</p>
+      <p>Learn more about our project and what we do here.</p>
+    </div>
+  )
+}
