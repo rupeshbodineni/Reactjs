29d76b6ec10dfdc9a5a274d0cd407b363fb0190f
rupeshbodineni <rupeshbodineni@gmail.com>
2026-07-30 21:48:59 +0530

    home page created

--- loginform/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="container mt-5">
-      <h1>Welcome to Home</h1>
-      <p>This is the home page of our application.</p>
-      <p>Navigate using the navbar to explore other pages.</p>
-    </div>
-  )
-}
